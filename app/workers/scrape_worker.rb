c9570c84cb4e97f8d2923666ad3ba076c295bf50
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-23 03:08:48 -0500

    jobs will fail gracefully if the song guid exists

--- app/workers/scrape_worker.rb
+++ app/workers/scrape_worker.rb
@@ -2,8 +2,10 @@ class ScrapeWorker
   include Sidekiq::Worker
 
   def perform(guid, artist_name, song_name)
-    Scrapes::SkateParts.new(
-      Song.new(guid: guid, name: song_name, artist_name: artist_name)
-    ).call
+    unless Song.find_by(guid: guid)
+      Scrapes::SkateParts.new(
+        Song.new(guid: guid, name: song_name, artist_name: artist_name)
+      ).call
+    end
   end
 end
