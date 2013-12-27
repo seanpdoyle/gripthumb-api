553c3be85013b4d0891aa7f16a472ef1c4c0bbb5
Sean Doyle <sean.p.doyle24@gmail.com>
2013-12-27 17:42:18 -0500

    set Controller asset path to S3 bucket

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Gripthumb::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['S3_BUCKET']}.s3.amazonaws.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
